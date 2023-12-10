7dca8a8c97abc606588dc7776b5a9f58364f7995
KZ211 <alexis.jares12@gmail.com>
2023-12-10 18:22:25 -0300

    Modificacion del body a 100% x 100%

--- src/components/layout/mainNavigation/MainNavigation.tsx
+++ src/components/layout/mainNavigation/MainNavigation.tsx
@@ -9,19 +9,15 @@ const MainNavigation : React.FC = () =>{
 
     return (
         <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
                 <h1><MdMessage/> Welcome</h1>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-            <Button icon={<MdPostAdd/>} name='New Post' interact={sayHello} />
-        </div>
-    </div>
-  </div>
-</nav>
+                <form className="d-flex" role="addPost">
+                    <Button icon={<MdPostAdd/>} name='New Post' interact={sayHello} />
+                </form>
+            </div>
+        </nav>
+        <hr className="hr"/>
         </>
     );
 }
